b2637f6f351c33a627ee0391abe7a31c5dafd37b
CR67 <romey.christophe@orange.fr>
2019-02-19 20:49:08 +0100

    Mise à jour du 19/02 : - Responsive web design : Tabs -> Accordion

--- js/activation.js
+++ js/activation.js
@@ -9,19 +9,19 @@ function setActive(nomClass)
         for (i = 0; i < tab1.length; i++) {
             tab1[i].classList.remove('is-active');
         }
-        tab1[0].firstElementChild.removeAttribute('aria-selected');
+        (tab1[0].firstElementChild).firstElementChild.removeAttribute('aria-selected');
     }else {
             var tab2=document.getElementsByClassName(nom);
             for (i=0; i<tab2.length; i++) {
                 tab2[i].classList.remove('is-active');
             }
-        tab2[0].firstElementChild.removeAttribute('aria-selected');
+        (tab2[0].firstElementChild).firstElementChild.removeAttribute('aria-selected');
         }
     var tab3=document.getElementsByClassName(nomClass);
     for (i=0; i<tab3.length; i++) {
         tab3[i].classList.add('is-active');
     }
-    tab3[0].firstElementChild.setAttribute('aria-selected', 'true');
+    (tab3[0].firstElementChild).firstElementChild.setAttribute('aria-selected', 'true');
     nom=nomClass;
 
     return;
--- view/layout.view.php
+++ view/layout.view.php
@@ -11,23 +11,28 @@ $vcontent = new $content['class']();
 <head>
 	<meta charset="utf-8" />
 	<title><?= $content['title'] ?></title>
-	<link rel="stylesheet" type="text/css" href="css/foundation.css" />
+    <link rel="stylesheet" type="text/css" href="css/foundation.css" />
+	<!--<link rel="stylesheet" type="text/css" href="css/foundationorigin.css" />-->
+    <link rel="stylesheet" type="text/css" href="css/app.css" />
 </head>
 
 	<body>
 
-	<header>
+	<header class="grid-container">
 		<?php $vheader->showHeader() ?>
 	</header>
 
 	<div id="content">
-		<?php $vcontent->{$content['method']}($content['arg']) ?>
+		<?php $vcontent->{$content['method']}($content['arg'])?>
 	</div><!-- id="content" -->
 
 		<script src="js/ajax.js"></script>
         <script src="js/activation.js"></script>
 		<script src="js/init.js"></script>
         <script src="js/jquery.js"></script>
-
+        <script src="js/foundation.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
 	</body>
 </html>
