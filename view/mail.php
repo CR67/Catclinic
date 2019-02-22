ba6a8b5e22ad70017599a4168bc09e3ddd34f212
CR67 <romey.christophe@orange.fr>
2019-02-22 11:01:59 +0100

    Mise à jour du 22/02 : - Mise en fonction du formulaire de contact (mail.php) - Configuration du serveur pour envoie mail (Sendmail plugin pour php) - Mise en place BD (BD 'catclinic' et table 'mail') pour enregistrement mail newsletter sur MySQL

--- view/mail.php
+++ view/mail.php
@@ -1,6 +1,28 @@
 
 <?php
 if(isset($_POST['message'])){
+
+    $nom = htmlentities($_POST['nom'], ENT_QUOTES);
+    $prenom = htmlentities($_POST['prenom'], ENT_QUOTES);
+    $mail = htmlentities($_POST['mail'], ENT_QUOTES);
+
+    if(isset($_POST['souscrire'])) {
+        try {
+            $bdd = new PDO('mysql:host=localhost; dbname=catclinic', 'root', 'NCC1701a');
+            $stmt1 = $bdd->prepare('SELECT adresse FROM mail WHERE adresse=:adresse');
+            $stmt1->execute(array('adresse' => $mail));
+            if ($stmt1->rowCount() != 0) {
+                echo '<script>alert("Vous avez déjà souscrit à la newsletter!");</script>';
+            } else {
+                $stmt2 = $bdd->prepare('INSERT INTO mail (nom, prenom, adresse) VALUES (:nom, :prenom, :adresse)');
+                $stmt2->execute(array('nom' => $nom, 'prenom' => $prenom, 'adresse' => $mail));
+            }
+        } catch (PDOException $ex) {
+            print"Erreur de connexion : " . $ex->getMessage() . "</br>";
+            die();
+        }
+    }
+
     $entete = 'MIME-version: 1.0'."\r\n";
     $entete .= 'Content-type: text/html; charset=utf-8'."\r\n";
     $entete .= 'From: '.$_POST['mail']."\r\n";
@@ -12,12 +34,12 @@ if(isset($_POST['message'])){
                 <br>
                 <p>'.$_POST['message'].'</p>';
 
-    $test = mail('romey.christophe@orange', $_POST['sujet'], $contenu, $entete);
+    $test = mail('chris1967.1701@gmail.com', $_POST['sujet'], $contenu, $entete);
 
     if($test){
-        echo '<script>alert('.'Message envoyè !'.');</script>';
+        echo '<script>alert("Message envoyè !");</script>';
     }else{
-        echo '<script>alert('.'Message non envoyé !'.');</script>';
+        echo '<script>alert("Message non envoyé !");</script>';
     }
 }
 ?>
